320244ca84aea7569746d86215eb03d180b4d6f3
Maksim Aniskov <MaksimAniskov@gmail.com>
2024-10-12 18:35:03 +0200

    setup.py files not in use anymore

--- plugins/file/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='File Plugin',
-    version='0.1.0',
-    install_requires=[
-    ]
-)
--- plugins/gitlab/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='GitLab Plugin',
-    version='0.1.0',
-    install_requires=[
-        'python-gitlab'
-    ]
-)
